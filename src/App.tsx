0d11f874d0cf79fc6a36416ee88bce507e7681b9
IvanEFan <ivan1efan@gmail.com>
2021-11-24 18:15:10 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <h1 className="flex items-center justify-center min-h-screen p-8 text-8xl text-center font-bold font-mono">🎉 Your project is bootstraped successfully with Essential Template</h1>
+    );
+};
+
+export default App;
